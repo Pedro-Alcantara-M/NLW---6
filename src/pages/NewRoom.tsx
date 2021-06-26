ad698b90a4a448ac977d72dd58a9b3e5e92318c6
Pedro-Alcantara-M <pedrinrn@gmail.com>
2021-06-26 08:10:57 -0300

    First Commit

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,63 @@
+import { FormEvent,useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+
+import ilustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import { Button } from '../components/Button'
+
+import "../styles/auth.scss"
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
+
+export function NewRoom() {
+  const { user } = useAuth()
+  const [newRom, setNewRoom] = useState('')
+  const history = useHistory()
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if (newRom.trim() === '') {
+      return;
+    }
+    
+    const roomRef = database.ref('rooms')
+
+    const firebaseRoom = await roomRef.push({
+      title: newRom,
+      authorId: user?.id,
+    })
+
+    history.push(`/room/${firebaseRoom.key}`)
+  }
+  
+  return(
+    <div id="page-auth">
+      <aside>
+        <img src={ilustrationImg} alt="Ilustração simbolizando perguntas e respostas"/>
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real </p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logomarca"/>
+          <h2>Criar uma nova sala</h2>
+          <form onSubmit={handleCreateRoom}>
+            <input 
+            type="text"
+            placeholder="Nome da sala"
+            onChange={event => setNewRoom(event.target.value)}
+            value={newRom}
+             />
+             <Button type='submit'>
+              Criar sala
+             </Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  )
+}
